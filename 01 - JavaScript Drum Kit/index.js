9cd4932e6ec370c6cc8965627327134d37950b75
SamOP549 <82203712+SamOP549@users.noreply.github.com>
2022-06-05 15:47:38 +0530

    Add files via upload

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('keydown', function (e){
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio)  return;
+
+    key.classList.add('playing');
+    audio.currentTime=0;
+    audio.play();
+});
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key=>key.addEventListener('transitionend', function (e){
+    if(e.propertyName!== 'transform')   return;
+    e.target.classList.remove('playing');
+}))
